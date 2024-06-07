546eeb25140934e08fa5f6cf902437195f39bbe5
satyachiratlae <satyachiratla77@gmail.comm>
2024-06-07 10:03:35 +0530

    mobile-nav fixed

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -9,7 +9,7 @@ type MobileNavProps = {
 
 export default function MobileNav({ toggleHandler }: MobileNavProps) {
   return (
-    <header className="bg-black fixed top-0 left-0 right-0 bottom-0 h-full p-6 pt-16">
+    <header className="bg-black fixed top-0 left-0 right-0 bottom-0 h-full p-6 pt-16 z-50">
       <RxCross2
         size={25}
         onClick={toggleHandler}
@@ -17,16 +17,19 @@ export default function MobileNav({ toggleHandler }: MobileNavProps) {
       />
       <nav className="bg-white h-full p-4 rounded-lg z-50">
         <ul className="flex flex-col gap-4">
-          <li>
+          <li onClick={toggleHandler}>
             <NavLink href="/">Home</NavLink>
           </li>
-          <li>
+          <li onClick={toggleHandler}>
             <NavLink href="/about">About</NavLink>
           </li>
-          <li>
+          <li onClick={toggleHandler}>
             <NavLink href="/services">Services</NavLink>
           </li>
-          <li className="cursor-pointer text-white bg-blue-900 hover:bg-sky-600 rounded-full px-5 py-2 translate-all duration-200">
+          <li
+            onClick={toggleHandler}
+            className="cursor-pointer text-white bg-blue-900 hover:bg-sky-600 rounded-full px-5 py-2 translate-all duration-200"
+          >
             <Link href="/contact">Contact us</Link>
           </li>
         </ul>
